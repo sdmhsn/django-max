c31a184cb3159702b37171525b38725f6931d93f
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-10 14:11:26 +0800

    section 4

---
+++ 02 templates & static files/monthly_challenges/challenges/views.py
@@ -0,0 +1,50 @@
+from urllib import response
+from django.shortcuts import render
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+
+# Create your views here.
+monthly_challenges = {
+    'january': 'Eat no meat for the entire month',
+    'february': 'Walk for at least 30 minutes every day',
+    'march': 'Learn Django at least 20 minutes every day',
+    'april': 'Eat no meat for the entire month',
+    'may': 'Walk for at least 30 minutes every day',
+    'june': 'Learn Django at least 20 minutes every day',
+    'july': 'Eat no meat for the entire month',
+    'august': 'Walk for at least 30 minutes every day',
+    'september': 'Learn Django at least 20 minutes every day',
+    'october': 'Eat no meat for the entire month',
+    'november': 'Walk for at least 30 minutes every day',
+    'december': None,
+}
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound('Invalid month number!')
+
+    redirect_month = months[month - 1]
+    month_path = reverse('month-challenge', args=[redirect_month])
+    return HttpResponseRedirect(month_path)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return render(request, 'challenges/challenge.html', {
+            'text': challenge_text,
+            'month_name': month,
+        })
+    except:
+        raise Http404()
+
+
+def index(request):
+    months = list(monthly_challenges.keys())
+
+    return render(request, 'challenges/index.html', {'months': months})
