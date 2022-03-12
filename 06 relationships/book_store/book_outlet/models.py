28cce716aebfc3f234bdb97eb42fc6efeb55b01c
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-13 00:20:29 +0800

    section 8: cross model queries

--- 06 relationships/book_store/book_outlet/models.py
+++ 06 relationships/book_store/book_outlet/models.py
@@ -16,7 +16,7 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     # author = models.CharField(null=True, max_length=100)
     author = models.ForeignKey(
-        Author, on_delete=models.CASCADE, null=True)  # to connect Author model to Book model (one to many relation)
+        Author, on_delete=models.CASCADE, null=True, related_name='books')  # to connect Author model to Book model (one to many relation)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default='', blank=True, null=False, db_index=True)
 
