a860dc238b2e840d728daedc933c280ab22260df
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-15 00:20:28 +0800

    section 9: author model & one-to-many

--- 07 building a blog: data and models/my_site/blog/models.py
+++ 07 building a blog: data and models/my_site/blog/models.py
@@ -1,8 +1,15 @@
+from multiprocessing import AuthenticationError
 from django.db import models
 from django.core.validators import MinLengthValidator
 
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=254)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -10,3 +17,4 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)  # unique=True: I want to use the slug as a unique identifier for getting my posts. db_index=True: to let Django and Sequel trade an index for that field to make querying and filtering based on it a bit more effective.
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts')
