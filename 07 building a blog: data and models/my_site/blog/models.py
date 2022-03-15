679cda032ee942baaa0b1bf7751e446a578db8db
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-15 11:40:33 +0800

    section 9: adding data via admin

--- 07 building a blog: data and models/my_site/blog/models.py
+++ 07 building a blog: data and models/my_site/blog/models.py
@@ -7,11 +7,20 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.caption
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField(max_length=254)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
 
 
 class Post(models.Model):
@@ -21,5 +30,6 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)  # unique=True: I want to use the slug as a unique identifier for getting my posts. db_index=True: to let Django and Sequel trade an index for that field to make querying and filtering based on it a bit more effective.
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     tags = models.ManyToManyField(Tag)
