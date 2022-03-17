e507ad86a37254dd268ddafba2835d0d8b170cda
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-17 12:07:51 +0800

    section 10: handling form submission & extracting data (returning a different render result in if block)

--- 08 forms/feedback/reviews/views.py
+++ 08 forms/feedback/reviews/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return render(request, 'reviews/thank_you.html')
+
     return render(request, 'reviews/review.html')
