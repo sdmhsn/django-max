9798c94a4a790efa634a89e5c7b9bf61494187c6
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-17 18:07:42 +0800

    section 10: manual form validation & the problems with "that"

--- 08 forms/feedback/reviews/views.py
+++ 08 forms/feedback/reviews/views.py
@@ -7,9 +7,13 @@ def index(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
         print(entered_username)
+
+        if entered_username == '':
+            return render(request, 'reviews/review.html', {'has_error': True}) # {'has_error': True}: extra context pass to the template
+
         return HttpResponseRedirect('/thank-you')
 
-    return render(request, 'reviews/review.html')
+    return render(request, 'reviews/review.html', {'has_error': False}) # {'has_error': False}: extra context pass to the template
 
 
 def thank_you(request):
