5070ee94e0dbb1ba22d783f8cb6d0349242461eb
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-23 00:23:25 +0800

    section 10: class based views

--- 08 forms/feedback/reviews/views.py
+++ 08 forms/feedback/reviews/views.py
@@ -1,43 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 
 from .forms import ReviewForm
-from .models import Review
 
 
 # Create your views here.
-def index(request):
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, 'reviews/review.html', {'form': form})
+
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            # print(form.cleaned_data['user_name'])
-            '''
-            # experiment:
-            Review.objects.create(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating']
-            )
-            '''
-
-            # video tutorial:
-            # review = Review(
-            #     user_name=form.cleaned_data['user_name'],
-            #     review_text=form.cleaned_data['review_text'],
-            #     rating=form.cleaned_data['rating']
-            # )
-            # review.save()
-
-            # since this is a ModelForm, we can now call save() on it, that will then save that data to the database
             form.save() 
             
             return HttpResponseRedirect('/thank-you')
 
-    else:
-        form = ReviewForm()
+        return render(request, 'reviews/review.html', {'form': form})
 
-    return render(request, 'reviews/review.html', {'form': form})
+
+# def index(request):
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
+
+#         if form.is_valid():
+#             form.save() 
+
+#             return HttpResponseRedirect('/thank-you')
+
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, 'reviews/review.html', {'form': form})
 
 
 def thank_you(request):
