446b1049d9464a45ce30904f77c193e15625bff1
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-22 13:15:39 +0800

    section 10: introducing ModelForm

--- 08 forms/feedback/reviews/forms.py
+++ 08 forms/feedback/reviews/forms.py
@@ -1,9 +1,20 @@
+# from django import forms
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label='Your Name', max_length=100, error_messages={
+#         'required': 'Your name must not be empty',
+#         'max_length': 'Please enter a shorter name!'
+#     })
+#     review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField()
+
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label='Your Name', max_length=100, error_messages={
-        'required': 'Your name must not be empty',
-        'max_length': 'Please enter a shorter name!'
-    })
-    review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField()
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        exclude = ['user_name']
