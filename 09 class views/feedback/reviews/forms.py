c974ea0ab4ed0f34834981398745c738fad61749
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-23 11:07:47 +0800

    section 11: adding templates

---
+++ 09 class views/feedback/reviews/forms.py
@@ -0,0 +1,34 @@
+# from django import forms
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label='Your Name', max_length=100, error_messages={
+#         'required': 'Your name must not be empty',
+#         'max_length': 'Please enter a shorter name!'
+#     })
+#     review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField()
+
+from django import forms
+
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        # exclude = ['user_name']
+        labels = {
+            'user_name': 'Your Name',
+            'review_text': 'Your Feedback',
+            'rating': 'Rating',
+        }
+        error_messages = {
+            'user_name': { # error messages for user_name field
+                'required': 'Your name must not be empty',
+                'max_length': 'Please enter a shorter name!'
+            },
+            'review_text': {
+                'required': 'Field empty!',
+            }
+        }
---
+++ 09 class views/feedback/reviews/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    # path('', views.index),
+    path('', views.ReviewView.as_view()), # urlpattern required for class based view
+    path('thank-you', views.thank_you),
+]
