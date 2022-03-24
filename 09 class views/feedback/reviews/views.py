abc1804f870cb35d0bd5250a831a2bcf684a50ac
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-24 08:56:12 +0800

    section 11: the ListView

--- 09 class views/feedback/reviews/views.py
+++ 09 class views/feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -34,15 +35,16 @@ class ThankYouView(TemplateView):
         return context
 
 
-class ReviewListView(TemplateView):
+class ReviewListView(ListView):
     template_name = 'reviews/review_list.html'
+    model = Review
+    context_object_name = 'reviews'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context = {'reviews': reviews, 'title': 'All Reviews'} # experiment: multiple context for cbv
-        return context
-
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=3)  # rating by greater than 3
+        return data
+    
 
 class SingleReviewView(TemplateView):
     template_name = 'reviews/single_review.html'
