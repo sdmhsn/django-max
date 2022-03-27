98a62822f4b7f374386760224947af8c3b04e9b5
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-27 14:26:53 +0800

    section 12: storing uploaded files native approach

--- 10 file uploads/feedback/profiles/views.py
+++ 10 file uploads/feedback/profiles/views.py
@@ -4,10 +4,16 @@ from django.views import View
 
 
 # Create your views here.
+def store_file(file):
+    with open('temp/image.jpg', 'wb+') as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
         return render(request, 'profiles/create_profile.html')
 
     def post(self, request):
-        print(request.FILES['imageq'])
+        store_file(request.FILES['imageq'])
         return HttpResponseRedirect('/profiles')
