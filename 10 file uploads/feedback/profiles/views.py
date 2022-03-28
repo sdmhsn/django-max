5ab06c2c5b8accac8c2d2ed96d4e22d017c0bc03
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-28 23:24:46 +0800

    section 12: using a CreateView()

--- 10 file uploads/feedback/profiles/views.py
+++ 10 file uploads/feedback/profiles/views.py
@@ -1,30 +1,38 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
+# from django.http import HttpResponseRedirect
+# from django.shortcuts import render
+# from django.views import View
+from django.views.generic import CreateView
 
-from .forms import ProfileForm
+# from .forms import ProfileForm
 from .models import UserProfile
 
 
 # Create your views here.
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, 'profiles/create_profile.html', {'form': form})
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/profiles'
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
 
-        if submitted_form.is_valid():
-            # video tutorial:
-            profile = UserProfile(image=request.FILES['user_image'])
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, 'profiles/create_profile.html', {'form': form})
 
-            # experiment:
-            # profile = UserProfile(
-            #     image=submitted_form.cleaned_data['user_image']
-            # )
-            profile.save()
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
 
-            return HttpResponseRedirect('/profiles')
+#         if submitted_form.is_valid():
+#             # video tutorial:
+#             profile = UserProfile(image=request.FILES['user_image'])
 
-        return render(request, 'profiles/create_profile.html', {'form': submitted_form})
+#             # experiment:
+#             # profile = UserProfile(
+#             #     image=submitted_form.cleaned_data['user_image']
+#             # )
+#             profile.save()
+
+#             return HttpResponseRedirect('/profiles')
+
+#         return render(request, 'profiles/create_profile.html', {'form': submitted_form})
