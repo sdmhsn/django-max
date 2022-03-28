f501816ae4bea4efb85bf3cb68d0ae3c9bd36971
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-28 15:42:01 +0800

    section 12: using models for file storage

--- 10 file uploads/feedback/profiles/views.py
+++ 10 file uploads/feedback/profiles/views.py
@@ -3,15 +3,10 @@ from django.shortcuts import render
 from django.views import View
 
 from .forms import ProfileForm
+from .models import UserProfile
 
 
 # Create your views here.
-def store_file(file):
-    with open('temp/image.jpg', 'wb+') as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
-
-
 class CreateProfileView(View):
     def get(self, request):
         form = ProfileForm()
@@ -21,7 +16,15 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
 
         if submitted_form.is_valid():
-            store_file(request.FILES['imageq'])
+            # video tutorial:
+            profile = UserProfile(image=request.FILES['user_image'])
+
+            # experiment:
+            # profile = UserProfile(
+            #     image=submitted_form.cleaned_data['user_image']
+            # )
+            profile.save()
+
             return HttpResponseRedirect('/profiles')
 
         return render(request, 'profiles/create_profile.html', {'form': submitted_form})
