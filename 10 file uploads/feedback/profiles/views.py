7736054efeb52c951b691246cd50aab21d476cdd
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-29 23:10:25 +0800

    section 12: working with the file field

--- 10 file uploads/feedback/profiles/views.py
+++ 10 file uploads/feedback/profiles/views.py
@@ -2,6 +2,7 @@
 # from django.shortcuts import render
 # from django.views import View
 from django.views.generic import CreateView
+from django.views.generic import ListView
 
 # from .forms import ProfileForm
 from .models import UserProfile
@@ -15,24 +16,7 @@ class CreateProfileView(CreateView):
     success_url = '/profiles'
 
 
-# class CreateProfileView(View):
-#     def get(self, request):
-#         form = ProfileForm()
-#         return render(request, 'profiles/create_profile.html', {'form': form})
-
-#     def post(self, request):
-#         submitted_form = ProfileForm(request.POST, request.FILES)
-
-#         if submitted_form.is_valid():
-#             # video tutorial:
-#             profile = UserProfile(image=request.FILES['user_image'])
-
-#             # experiment:
-#             # profile = UserProfile(
-#             #     image=submitted_form.cleaned_data['user_image']
-#             # )
-#             profile.save()
-
-#             return HttpResponseRedirect('/profiles')
-
-#         return render(request, 'profiles/create_profile.html', {'form': submitted_form})
+class ProfileView(ListView):
+    template_name = 'profiles/user_profiles.html'
+    model = UserProfile
+    context_object_name = 'profiles'
