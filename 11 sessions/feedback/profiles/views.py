fb3fc1498d13eb1300945a8f4192da1f3d840aac
Saddam Husein <saddambinhusein2@gmail.com>
2022-03-30 10:56:20 +0800

    section 13: enabling & configuring sessions (SESSION_COOKIE_AGE)

---
+++ 11 sessions/feedback/profiles/views.py
@@ -0,0 +1,22 @@
+# from django.http import HttpResponseRedirect
+# from django.shortcuts import render
+# from django.views import View
+from django.views.generic import CreateView
+from django.views.generic import ListView
+
+# from .forms import ProfileForm
+from .models import UserProfile
+
+
+# Create your views here.
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/profiles'
+
+
+class ProfileView(ListView):
+    template_name = 'profiles/user_profiles.html'
+    model = UserProfile
+    context_object_name = 'profiles'
---
+++ 11 sessions/feedback/reviews/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.ReviewView.as_view()), # urlpattern required for class based view
+    path('thank-you', views.ThankYouView.as_view()),
+    path('review-list', views.ReviewListView.as_view()),
+    path('review/<int:pk>', views.SingleReviewView.as_view()),
+]
