8d1aa2911ed54589ffb82344e8fc0ba2e5154b69
Saddam Husein <saddambinhusein2@gmail.com>
2022-04-03 22:59:33 +0800

    section 14: converting views to class based

--- 12 building a blog (forms, files and sessions)/my_site/blog/urls.py
+++ 12 building a blog (forms, files and sessions)/my_site/blog/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.starting_page, name='starting-page'),
-    path('posts/', views.posts, name='posts-page'),
-    path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),
+    path('', views.StartingPageView.as_view(), name='starting-page'),
+    # path('', views.starting_page, name='starting-page'),
+    path('posts/', views.PostsView.as_view(), name='posts-page'),
+    # path('posts/', views.posts, name='posts-page'),
+    path('posts/<slug:slug>', views.PostDetailView.as_view(), name='post-detail-page'),
+    # path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),
 ]
--- 12 building a blog (forms, files and sessions)/my_site/blog/views.py
+++ 12 building a blog (forms, files and sessions)/my_site/blog/views.py
@@ -1,25 +1,57 @@
-from django.shortcuts import render, get_object_or_404
+# from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-    # print(latest_posts)
-    return render(request, 'blog/index.html', {'posts': latest_posts})
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    context_object_name = 'posts'
+    # ordering = ['-date']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset.order_by('-date')[:3] # my code experiment. combined between ordering -date and slicing
+        return data
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by('-date')[:3]
+#     return render(request, 'blog/index.html', {'posts': latest_posts})
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, 'blog/all-posts.html', {'all_posts': all_posts})
+class PostsView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    # ordering = ['-date']
+    context_object_name = 'all_posts'
+
+    def get_queryset(self): # my code experiment for ordering the post by -date
+        base_query = super().get_queryset()
+        data = base_query.order_by('-date')
+        return data
+
+# def posts(request):
+    # all_posts = Post.objects.all().order_by('-date')
+    # return render(request, 'blog/all-posts.html', {'all_posts': all_posts})
 
 
-def post_detail(request, slug):
-    # identified_post = Post.objects.get(slug=slug)
-    identified_post = get_object_or_404(Post, slug=slug)  # we use include 404 page if it does fail
-    return render(request, 'blog/post-detail.html', 
-        {
-            'post': identified_post,
-            'post_tags': identified_post.tags.all()
-        }
-    )
+class PostDetailView(DetailView):
+    template_name = 'blog/post-detail.html'
+    model = Post  # It will also automatically raise a 404 error
+
+    # context for post_tags
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
+        return context
+
+# def post_detail(request, slug):
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     # identified_post = Post.objects.get(slug=slug)
+#     return render(request, 'blog/post-detail.html', 
+#         {
+#             'post': identified_post,
+#             'post_tags': identified_post.tags.all()
+#         }
+#     )
