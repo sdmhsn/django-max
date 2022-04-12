a9f7c28721e9c3ddf31e3f9cdf9740dc86b7e150
Saddam Husein <saddambinhusein2@gmail.com>
2022-04-12 23:51:25 +0800

    section 14: finishing the read later feature

--- 12 building a blog (forms, files and sessions)/my_site/blog/views.py
+++ 12 building a blog (forms, files and sessions)/my_site/blog/views.py
@@ -41,15 +41,28 @@ class PostsView(ListView):
 
 
 class PostDetailView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get('stored_posts')
+
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
         # post = get_object_or_404(Post, slug=slug)  # my code experiment. included 404 error page
+        
         context = {
             'post': post,
             'post_tags': post.tags.all(),
             'comment_form': CommentForm(),
-            'comments': post.comments.all().order_by('-id')  # comments: post field with related_name='comments' in Comment models 
+            'comments': post.comments.all().order_by('-id'),  # comments: post field with related_name='comments' in Comment models 
+            'saved_for_later': self.is_stored_post(request, post.id),
         }
+
         return render(request, 'blog/post-detail.html', context)
 
     def post(self, request, slug):
@@ -68,7 +81,8 @@ class PostDetailView(View):
             'post': post,
             'post_tags': post.tags.all(),
             'comment_form': comment_form,
-            'comments': post.comments.all().order_by('-id')  # comments: post field with related_name='comments' in Comment models 
+            'comments': post.comments.all().order_by('-id'),  # comments: post field with related_name='comments' in Comment models 
+            'saved_for_later': self.is_stored_post(request, post.id),
         }
         return render(request, 'blog/post-detail.html', context)
 
@@ -86,7 +100,7 @@ class PostDetailView(View):
 class ReadLaterView(View):
     def get(self, request):
         stored_posts = request.session.get('stored_posts')
-
+        
         context = {}
 
         if stored_posts is None or len(stored_posts) == 0:
@@ -109,6 +123,9 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session['stored_posts'] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+        
+        request.session['stored_posts'] = stored_posts
 
         return HttpResponseRedirect('/')
