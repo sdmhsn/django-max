50f3578d64cb7b4340d9a3e52fcb3f56a0b1ac29
Saddam Husein <saddambinhusein2@gmail.com>
2022-04-12 14:26:17 +0800

    section 14: read later page & styling

--- 12 building a blog (forms, files and sessions)/my_site/blog/views.py
+++ 12 building a blog (forms, files and sessions)/my_site/blog/views.py
@@ -93,12 +93,11 @@ class ReadLaterView(View):
             context['posts'] = []
             context['has_posts'] = False
         else:
-            posts = Post.objects.filter(id__in=stored_posts)
+            posts = Post.objects.filter(id__in=stored_posts).order_by('id')
             context['posts'] = posts
             context['has_posts'] = True
 
         return render(request, 'blog/stored-posts.html', context)
-
 
     def post(self, request):
         stored_posts = request.session.get('stored_posts')
