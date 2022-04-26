c76a05c197a6d92684f3fa9c71efdd78dfa5adbc
Saddam Husein <saddambinhusein2@gmail.com>
2022-04-26 18:36:32 +0800

    section 16: using what we learned

--- 14 django summary and quick introduction/django_course_site/meetups/views.py
+++ 14 django summary and quick introduction/django_course_site/meetups/views.py
@@ -4,8 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     meetups = [
-        {'title': 'A first meetup'},
-        {'title': 'A second meetup'},
+        {
+            'title': 'A First Meetup',
+            'location': 'New York',
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup',
+            'location': 'Paris',
+            'slug': 'a-second-meetup'
+        },
     ]
     return render(request, 'meetups/index.html', {
         'meetups': meetups,
