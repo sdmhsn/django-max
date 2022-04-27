665ad2f11cf0404fd038854769d346fedaf881a3
Saddam Husein <saddambinhusein2@gmail.com>
2022-04-27 12:44:40 +0800

    section 16: adding a detail page

--- 14 django summary and quick introduction/django_course_site/meetups/views.py
+++ 14 django summary and quick introduction/django_course_site/meetups/views.py
@@ -19,3 +19,14 @@ def index(request):
         'meetups': meetups,
         'show_meetups': True
     })
+
+
+def meetup_details(request):
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first meetup!'
+    }
+    return render(request, 'meetups/meetup_details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
