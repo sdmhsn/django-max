2418cf1ca82fcc80dd999f2b7661588ac8d595bb
Saddam Husein <saddambinhusein2@gmail.com>
2022-05-05 23:35:18 +0800

    section 16: creating a ModelForm

---
+++ 14 django summary and quick introduction/django_course_site/meetups/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Participant
+
+
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Participant
+        fields = ['email']
+        labels = {'email': 'Email'}
--- 14 django summary and quick introduction/django_course_site/meetups/views.py
+++ 14 django summary and quick introduction/django_course_site/meetups/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Meetup
+from .forms import RegistrationForm
 
 
 # Create your views here.
@@ -14,9 +15,11 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        registration_form = RegistrationForm()
         return render(request, 'meetups/meetup-detail.html', {
             'meetup_found': True,
             'meetup': selected_meetup,
+            'form': registration_form
         })
     except Exception as exc:
         return render(request, 'meetups/meetup-detail.html', {
