f292ced12e371c2e8795a1b42c84cb283a2f3e48
Saddam Husein <saddambinhusein2@gmail.com>
2022-05-05 14:37:28 +0800

    section 16: many to many relations

--- 14 django summary and quick introduction/django_course_site/meetups/models.py
+++ 14 django summary and quick introduction/django_course_site/meetups/models.py
@@ -12,9 +12,17 @@ class Location(models.Model):
         return f'{self.name} ({self.address})'
 
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
