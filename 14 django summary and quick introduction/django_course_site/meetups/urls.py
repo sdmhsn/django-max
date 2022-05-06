59221993412bf03837ff7cafa96ec4b059d2bc5d
Saddam Husein <saddambinhusein2@gmail.com>
2022-05-07 01:24:41 +0800

    section 16: polishing organizer email

--- 14 django summary and quick introduction/django_course_site/meetups/urls.py
+++ 14 django summary and quick introduction/django_course_site/meetups/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('meetups/', views.index, name='all-meetups'),  # our-domain.com/meetups/
     path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-detail'),  # our-domain.com/meetups/<dynamic-path-segment>
-    path('meetups/success', views.confirm_registration, name='confirm-registeration'),
+    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registeration'),
 ]
 
 
--- 14 django summary and quick introduction/django_course_site/meetups/views.py
+++ 14 django summary and quick introduction/django_course_site/meetups/views.py
@@ -23,7 +23,7 @@ def meetup_details(request, meetup_slug):
                 user_email = registration_form.cleaned_data['email']
                 participant, _ = Participant.objects.get_or_create(email=user_email)
                 selected_meetup.participants.add(participant)
-                return redirect('confirm-registeration')
+                return redirect('confirm-registeration', meetup_slug=meetup_slug)
 
         return render(request, 'meetups/meetup-detail.html', {
             'meetup_found': True,
@@ -36,5 +36,6 @@ def meetup_details(request, meetup_slug):
         })
 
 
-def confirm_registration(request):
-    return render(request, 'meetups/registration-success.html')
+def confirm_registration(request, meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(request, 'meetups/registration-success.html', {'organizer_email': meetup.organizer_emaill})
