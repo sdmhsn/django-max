349badec482b001789fbd32543a908c649c9e5ea
Saddam Husein <saddambinhusein2@gmail.com>
2022-05-07 20:15:09 +0800

    section 16: optimizing URLs

--- 14 django summary and quick introduction/django_course_site/meetups/urls.py
+++ 14 django summary and quick introduction/django_course_site/meetups/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'),  # our-domain.com/meetups/
-    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-detail'),  # our-domain.com/meetups/<dynamic-path-segment>
-    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registeration'),
+    path('', views.index, name='all-meetups'),  # our-domain.com/meetups/
+    path('<slug:meetup_slug>/', views.meetup_details, name='meetup-detail'),  # our-domain.com/meetups/<dynamic-path-segment>
+    path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registeration'),
 ]
 
 
